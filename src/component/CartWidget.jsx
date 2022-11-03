f993cbed5cbc75b80b7262a776e32100c76470b1
Ramiro <ramifazio@gmail.com>
2022-11-03 11:14:13 -0300

    Sugerencias!

--- src/component/CartWidget.jsx
+++ src/component/CartWidget.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import './CartWidget.css';
-
+import React from "react";
+import "./CartWidget.css";
 
 const CartWidget = () => {
   return (
-<a className='cart' href='#'>
-<i class="bi bi-bag-check"></i>
+    <a className="cart" href="#">
+      <i class="bi bi-bag-check"></i>
     </a>
-  )
+  );
+};
 
-}
-
-export default CartWidget
+export default CartWidget;
